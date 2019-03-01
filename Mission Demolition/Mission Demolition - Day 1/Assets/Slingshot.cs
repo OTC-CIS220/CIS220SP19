cd2dc13b7c9c210b999522bb009beeddae2b0095
Riley <rileyre@otc.edu>
2019-03-01 10:58:51 -0600

    First Mission Demolition

--- Mission Demolition/Mission Demolition - Day 1/Assets/Slingshot.cs
+++ Mission Demolition/Mission Demolition - Day 1/Assets/Slingshot.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
     // class level variable
-    // compiler attribute - gives specific instructions to the compiler or Unity
-    // tells Unity to create a header in the Inspector
     [Header("Set in Inspector")]
-    public GameObject prefabProjectile;     // set in Inspector
+    public GameObject prefabProjectile;
     public float velocityMult = 8f;
 
     [Header("Set dynamically")]
-    public GameObject launchPoint;          // 3d world pos of launchPoint
+    public GameObject launchPoint;
 
     public Vector3 launchPos;
     public GameObject projectile;
@@ -30,35 +28,6 @@ public class Slingshot : MonoBehaviour {
     {
         // if slingshot is not in aimingMode, don't run the code
         if (!aimingMode) return;        // bad code, bad
-
-        // get the current mouse position in 2D screen coordinates
-        Vector3 mousePos2D = Input.mousePosition;
-        mousePos2D.z = -Camera.main.transform.position.z;
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
-
-        // find the delta from the launchPos to the mousePos3D
-        Vector3 mouseDelta = mousePos3D - launchPos;        // vector subtraction
-        // Limit mouseDelta to the radius of the slingshot SphereCollider
-        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
-        if (mouseDelta.magnitude > maxMagnitude)
-        {
-            mouseDelta.Normalize();
-            mouseDelta *= maxMagnitude;
-        }  // end of if
-
-        // move the projectile to this new position
-        Vector3 projPos = launchPos + mouseDelta;       // vector addition
-        projectile.transform.position = projPos;        // move our projectile to the new position
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            // the mouse has been released
-            aimingMode = false;
-            projectileRigidbody.isKinematic = false;
-            projectileRigidbody.velocity = -mouseDelta * velocityMult;
-            FollowCam.POI = projectile;         // set POI for our camera
-            projectile = null;
-        }  // end of if
     }
     void OnMouseDown()
     {
---
+++ Mission Demolition/Mission Demolition/Assets/FollowCam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;       // the static point of interest
+                                        // static = singleton
+    [Header("Set in Inpsector")]
+    public float easing = 0.05f;
+
+    public Vector2 minXY = new Vector2(0,0);
+
+    [Header("Set Dynamically")]
+    public float camZ;                   // desired Z pos of the camera
+	// Use this for initialization
+	void Awake ()
+	{
+	    camZ = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    if (POI == null) return;        // get out of this method, no POI
+
+        // get the position of the poi
+	    Vector3 destination = POI.transform.position;
+        // limit the x & y to minimum values
+	    destination.x = Mathf.Max(minXY.x, destination.x);
+	    destination.y = Mathf.Max(minXY.y, destination.y);
+        // interpolate from the current Camera position toward the destition
+	    destination = Vector3.Lerp(transform.position, destination, easing);
+        // force destination.z to be camZ to keep the camera far enough away
+	    destination.z = camZ;
+	    transform.position = destination;   // update camera position
+	    Camera.main.orthographicSize = destination.y + 10;
+	}
+}
