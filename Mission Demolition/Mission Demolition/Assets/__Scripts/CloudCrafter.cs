e9ff6988d9003d31f17f4d65d5e44ad0352baa90
Riley <rileyre@otc.edu>
2019-03-08 10:50:34 -0600

    Clouds are complete. First castle developed.

---
+++ Mission Demolition/Mission Demolition/Assets/__Scripts/CloudCrafter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int numClouds = 40;          // the # of clouds to make
+    public GameObject cloudPrefab;      // cloud prefab
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public Vector3 zeroVal = new Vector3(0, 0, 0);
+    public Vector3 oneVal = new Vector3(1, 1, 1);
+    public float cloudScaleMin = .25f;
+    public float cloudScaleMax = 1f;
+    public float cloudSpeedMult = 0.5f;
+
+    public GameObject[] cloudInstances;
+
+    // Use this for initialization
+    void Awake()
+    {
+        // make an array large enough to hold all our cloud instances
+        cloudInstances = new GameObject[numClouds];
+        // find the cloudAnchor parent GameObject
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        // iterate through and make cloud instances
+        for (int i = 0; i < numClouds; i++)
+        {
+            // make an instance of our cloud prefab
+            GameObject cloud = Instantiate<GameObject>(cloudPrefab);
+            // position cloud
+            Vector3 cPos = zeroVal;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+            // scale cloud
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+            // smaller clouds should be further away
+            cPos.z = 100 - 90 * scaleU;
+            // apply these transforms to the cloud
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = oneVal * scaleVal;
+            // make cloud a child of the anchor
+            cloud.transform.SetParent(anchor.transform);
+            cloudInstances[i] = cloud;
+
+        }  // end of for loop
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Iterate over each cloud that was created
+        foreach (GameObject cloud in cloudInstances)
+        {
+            // get the cloud scale and position
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cPos = cloud.transform.position;
+            // move larger clouds faster
+            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
+            // if a cloud has moved too far to the left
+            if (cPos.x <= cloudPosMin.x)
+            {
+                cPos.x = cloudPosMax.x;
+            }
+
+            cloud.transform.position = cPos;
+        }
+    }
+}
---
+++ Mission Demolition/Mission Demolition/Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    // class level variable
+    // compiler attribute - gives specific instructions to the compiler or Unity
+    // tells Unity to create a header in the Inspector
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;     // set in Inspector
+    public float velocityMult = 8f;
+
+    [Header("Set dynamically")]
+    public GameObject launchPoint;          // 3d world pos of launchPoint
+
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    } // end of Awake method
+
+    void Update()
+    {
+        // if slingshot is not in aimingMode, don't run the code
+        if (!aimingMode) return;        // bad code, bad
+
+        // get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;        // vector subtraction
+        // Limit mouseDelta to the radius of the slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }  // end of if
+
+        // move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;       // vector addition
+        projectile.transform.position = projPos;        // move our projectile to the new position
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            // the mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;         // set POI for our camera
+            projectile = null;
+        }  // end of if
+    }
+    void OnMouseDown()
+    {
+        // the player has pressed the mouse button while over slingshot
+        aimingMode = true;
+        // instantiating a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // set it to isKinematic for now
+        //projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+	// Use this for initialization
+	void OnMouseEnter ()
+	{
+	    //print("Slingshot:OnMouseEnter()");
+	    launchPoint.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void OnMouseExit ()
+	{
+	    //print("Slingshot:OnMouseExit()");
+	    launchPoint.SetActive(false);
+	}
+}
